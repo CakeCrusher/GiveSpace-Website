3b60063009eea96524a5cf27f90c5bb57b695b2c
CakeCrusher <1sebastian1sosa1@gmail.com>
2021-12-14 18:24:15 -0500

    deploy ready

--- next.config.js
+++ next.config.js
@@ -1,14 +1,22 @@
 const webpack = require("webpack");
 
 const { parsed: myEnv } = require("dotenv").config();
-
-module.exports = {
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
-    return config;
-  },
-  reactStrictMode: true,
-  images: {
-    domains: ["m.media-amazon.com"],
-  },
-};
+try {
+  module.exports = {
+    webpack(config) {
+      config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+      return config;
+    },
+    reactStrictMode: true,
+    images: {
+      domains: ["m.media-amazon.com"],
+    },
+  };
+} catch {
+  module.exports = {
+    reactStrictMode: true,
+    images: {
+      domains: ["m.media-amazon.com"],
+    },
+  };
+}
