862fa6070d0a3f9dbb6328d0fc3784aee44b8f89
mhoobler <hoobler.michael@gmail.com>
2021-12-21 23:43:48 -0500

    fixed font

--- pages/list/[id].js
+++ pages/list/[id].js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../../styles/Home.module.css';
-import { useRouter } from 'next/router';
-import { fetchGraphQL } from '../../utils/helperFunctions';
-import { GET_LIST } from '../../utils/schemas';
+import React, { useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/Home.module.css";
+import { useRouter } from "next/router";
+import { fetchGraphQL } from "../../utils/helperFunctions";
+import { GET_LIST } from "../../utils/schemas";
 import {
   Text,
   Avatar,
@@ -22,12 +22,12 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-} from '@chakra-ui/react';
-import Item from '../../components/Item';
-import Nav from '../../components/Nav';
-import Link from 'next/link';
+} from "@chakra-ui/react";
+import Item from "../../components/Item";
+import Nav from "../../components/Nav";
+import Link from "next/link";
 
-import { FiShare2 } from 'react-icons/fi';
+import { FiShare2 } from "react-icons/fi";
 
 const List = ({ list }) => {
   const [copied, setCopied] = useState(false);
@@ -40,7 +40,7 @@ const List = ({ list }) => {
   }
 
   const GOOGLE_PLAY_LINK =
-    'https://play.google.com/store/apps/details?id=com.reservadex';
+    "https://play.google.com/store/apps/details?id=com.reservadex";
 
   const handleCopy = () => {
     setCopied(true);
@@ -63,69 +63,69 @@ const List = ({ list }) => {
   ));
 
   return (
-    <Box px="24px" minH="100vh" minW="100vw" bg="#e9e9e9">
-      <Flex direction="column">
+    <Box px='24px' minH='100vh' minW='100vw' bg='#e9e9e9'>
+      <Flex direction='column'>
         <Nav appLink={GOOGLE_PLAY_LINK} />
         <Head>
           <title>
             {list.user.username}&apos;s {list.title} wishlist
           </title>
           <meta
-            name="description"
+            name='description'
             content={`This list contains ${
               list.items.filter((item) => !item.status).length
             } unbought items.`}
           />
-          <link rel="icon" href="/favicon.ico" />
-          <meta property="og:site_icon" content="/favicon.ico" />
-          <meta property="og:site_name" content="GiveSpace" />
-          <meta property="og:image" content={list.user.profile_pic_url} />
-          <meta name="author" content={list.user.username} />
+          <link rel='icon' href='/favicon.ico' />
+          <meta property='og:site_icon' content='/favicon.ico' />
+          <meta property='og:site_name' content='GiveSpace' />
+          <meta property='og:image' content={list.user.profile_pic_url} />
+          <meta name='author' content={list.user.username} />
           <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
+            name='viewport'
+            content='width=device-width, initial-scale=1.0'
           />
-          <meta name="theme-color" content="#c9042c" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <meta name='theme-color' content='#c9042c' />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500;600&family=Source+Sans+Pro:wght@400;600&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css2?family=Quicksand:wght@500;600&family=Source+Sans+Pro:wght@400;600&display=swap'
+            rel='stylesheet'
           />
         </Head>
 
         <main className={styles.main}>
-          <Flex px={{ base: '0px', lg: '124px' }} direction="column">
-            <Flex w="100%" align="center">
+          <Flex px={{ base: "0px", lg: "124px" }} direction='column'>
+            <Flex w='100%' align='center'>
               <Avatar
-                w={{ base: '64px', lg: '128px' }}
-                h={{ base: '64px', lg: '128px' }}
-                name="Kola Tioluwani"
+                w={{ base: "64px", lg: "128px" }}
+                h={{ base: "64px", lg: "128px" }}
+                name='Kola Tioluwani'
                 src={list.user.profile_pic_url}
                 mr={4}
               />
-              <Flex direction="column">
-                <Text opacity={0.9} fontSize={{ md: '24px', base: '18px' }}>
+              <Flex direction='column'>
+                <Text opacity={0.9} fontSize={{ md: "24px", base: "18px" }}>
                   {list.user.username}
                 </Text>
-                <Text fontSize={{ md: '36px', base: '24px' }} fontWeight="600">
+                <Text fontSize={{ md: "36px", base: "24px" }} fontWeight='600'>
                   {list.title}
                 </Text>
               </Flex>
               <Spacer />
             </Flex>
-            <Flex mt="48px" mb="32px">
-              <Flex cursor="pointer" onClick={handleCopy}>
+            <Flex mt='48px' mb='32px'>
+              <Flex cursor='pointer' onClick={handleCopy}>
                 <Center>
                   <Icon m={1.5} boxSize={6} as={FiShare2} />
-                  <Text fontSize={{ base: '18px', lg: '24px' }}>
-                    {copied ? 'Link copied' : 'Share'}
+                  <Text fontSize={{ base: "18px", lg: "24px" }}>
+                    {copied ? "Link copied" : "Share"}
                   </Text>
                 </Center>
               </Flex>
               <Spacer />
             </Flex>
-            <Flex w="100%" justifyContent="flex-start" flexWrap="wrap">
+            <Flex w='100%' justifyContent='flex-start' flexWrap='wrap'>
               {items}
             </Flex>
           </Flex>
@@ -133,30 +133,30 @@ const List = ({ list }) => {
             <ModalOverlay />
             <ModalContent>
               <ModalCloseButton />
-              <ModalBody maxW="95%">
+              <ModalBody maxW='95%'>
                 <Flex>
                   <Center>
                     <ImageComponent
                       boxSize={42}
-                      objectFit="cover"
+                      objectFit='cover'
                       borderRadius={5}
-                      src="/icon.png"
-                      alt="GiveSpace icon"
+                      src='/icon.png'
+                      alt='GiveSpace icon'
                     />
                     <Flex>
                       <Text
-                        fontFamily="Quicksand"
-                        fontWeight="600"
-                        color="brand.900"
-                        fontSize="4xl"
+                        fontFamily='Quicksand'
+                        fontWeight='600'
+                        color='brand.900'
+                        fontSize='4xl'
                         ml={2}
                       >
                         Give
                       </Text>
                       <Text
-                        fontWeight="600"
-                        fontFamily="Quicksand"
-                        fontSize="4xl"
+                        fontWeight='600'
+                        fontFamily='Quicksand'
+                        fontSize='4xl'
                       >
                         Space
                       </Text>
@@ -167,12 +167,12 @@ const List = ({ list }) => {
               </ModalBody>
 
               <ModalFooter>
-                <Button variant="ghost" onClick={closeModal}>
+                <Button variant='ghost' onClick={closeModal}>
                   close
                 </Button>
                 <Link href={GOOGLE_PLAY_LINK}>
                   <a>
-                    <Button color="white" bg="brand.900" mr={3}>
+                    <Button color='white' bg='brand.900' mr={3}>
                       download
                     </Button>
                   </a>
@@ -193,7 +193,7 @@ export const getServerSideProps = async ({ params }) => {
     list_id: params.id,
   });
   if (fetchRes.errors || !fetchRes.data.list[0]) {
-    return { props: { list: { errors: 'List not found' } } };
+    return { props: { list: { errors: "List not found" } } };
   } else {
     return { props: { list: fetchRes.data.list[0] } };
   }
